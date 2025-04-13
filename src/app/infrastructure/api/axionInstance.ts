57c8a77b19a6568f9fa0256d98a458639565213e
David Mayo <davidmayopacheco@gmail.com>
2025-04-12 23:10:05 -0600

    feat(AxiosBearer) Implementacion de interceptor para enviar siempre el token

--- src/app/infrastructure/api/axionInstance.ts
+++ src/app/infrastructure/api/axionInstance.ts
@@ -1,7 +1,25 @@
 import axios from "axios"
+import { getDataCookies } from "../service/CookiesService";
+import { DataCookies } from "../../domain/models/cookies/DataCookies";
 
 export const axiosInstance = axios.create({
     // baseURL: 'https://t2orimport.meta.env.xjhjd3.execute-api.us-east-1.amazonaws.com', // Aquí pondrías tu endpoint real
     baseURL: "https://quickbite-apigateway.onrender.com", // Aquí pondrías tu endpoint real
 });
   
+
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        // Llamamos a una función asíncrona antes de continuar
+
+        const token = getDataCookies(DataCookies.ACCESSTOKEN);
+
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
