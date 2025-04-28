1cee92e218e59234be92f23b84cae7d24a1c5d5e
David Mayo <davidmayopacheco@gmail.com>
2025-04-27 22:42:32 -0600

    feat(redux) Implementacion de redux para carrito de compras

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { App } from './presentation/App'
 import theme from './theme'
+import { Provider } from 'react-redux'
+import { store } from './app/infrastructure/store/Store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* Maneja el modo de color inicial */}
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
 )
